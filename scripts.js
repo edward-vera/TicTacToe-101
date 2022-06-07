3e323876bf15f43ef20e2006caa88b1956d54a66
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-06-07 17:03:37 -0500

    final commit

--- scripts.js
+++ scripts.js
@@ -119,13 +119,22 @@ const verticalWin = () => {
   // Your code here to check for vertical wins
     if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
     || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-    ){return true}
+    )
+    {
+      return true
+    }
     if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
     || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
-    ){return true}    
+    )
+    {
+      return true
+    }    
     if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
     || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
-    ){return true}
+    )
+    {
+      return true
+    }
     return false
 }
 
@@ -133,9 +142,15 @@ const diagonalWin = () => {
   // Your code here to check for diagonal wins
     if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
     || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-    ){return true}
+    )
+    {
+      return true
+    }
     if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
     || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
-    ){return true}
+    )
+    {
+      return true
+    }
     return false
 }
